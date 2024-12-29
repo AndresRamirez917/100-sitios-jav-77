098a04f7f6da9cf6a62993a34c096d2657daad6a
AndresRamirez917 <andresmintic2021@gmail.com>
2024-12-29 13:31:34 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -2,12 +2,15 @@ async function getData() {
     const result = await fetch('https://randomuser.me/api?results=3');
     const character = await result.json();
     console.log(character);
+    console.log(character);
+
 }
 getData()
 
 async function getData2() {
     const result = await fetch('https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita');
     const coctail = await result.json()
+    console.log(coctail);
     console.log(coctail);
 }
 getData2()
