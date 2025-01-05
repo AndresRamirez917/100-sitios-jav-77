93bb4e12873f416db65de3d93e7b3846691658d5
AndresRamirez917 <andresmintic2021@gmail.com>
2025-01-05 17:41:22 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -1,12 +1,3 @@
-async function getData() {
-    const result = await fetch('https://randomuser.me/api?results=3');
-    const character = await result.json();
-    console.log(character);
-    console.log(character);
-
-}
-getData()
-
 async function getData2() {
     const result = await fetch('https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita');
     const coctail = await result.json()
@@ -14,6 +5,40 @@ async function getData2() {
     console.log(coctail);
 }
 getData2()
+
+async function getData() {
+    const result = await fetch('https://randomuser.me/api?results=3');
+    const character = await result.json();
+    console.log(character);
+    const profesionArr = ["ux fundamental","Modern HTML & CSS","Bootstrap 5"]
+    character.results.forEach((element, id) => {
+   
+            const box = document.createRange().createContextualFragment(`
+                
+                <div class="flexbox-1">
+                    <img src="${element.picture.large}" class="user rounded-circle" alt="">
+                    <div class="flexbox-1 test flex">
+                        <h4>${element.name.first} ${element.name.last}</h4>
+                    </div>
+                    <h2 class="display">${profesionArr[id]}</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis exercitationem dolore voluptas cupiditate alias nobis. Esse veritatis consequuntur tenetur adipisci.</p>
+                </div>
+                
+                `)
+                const courses_flex = document.querySelector('.courses-flex');
+                courses_flex.append(box)
+        
+    });
+
+}
+getData()
+
+
+
+
+
+
+
 
 
 
