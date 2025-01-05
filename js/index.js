ed3553541d529adf73ad52fb9dc925b04d85b3a0
AndresRamirez917 <andresmintic2021@gmail.com>
2025-01-05 17:54:31 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -9,13 +9,17 @@ getData2()
 async function getData() {
     const result = await fetch('https://randomuser.me/api?results=3');
     const character = await result.json();
+    const result2 = await fetch('https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita');
+    const coctail = await result2.json()
     console.log(character);
     const profesionArr = ["ux fundamental","Modern HTML & CSS","Bootstrap 5"]
+    const numbersArr = ["1", "2", "3"]
     character.results.forEach((element, id) => {
    
             const box = document.createRange().createContextualFragment(`
                 
                 <div class="flexbox-1">
+                 <img src="/images/course-${numbersArr[id]}.jpg" alt="">
                     <img src="${element.picture.large}" class="user rounded-circle" alt="">
                     <div class="flexbox-1 test flex">
                         <h4>${element.name.first} ${element.name.last}</h4>
